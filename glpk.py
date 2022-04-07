e012237e8ff8e8d314224c83fa67e1f9240d6c56
sverheek <sacha.verheeke@ulb.be>
2022-04-07 11:03:24 +0200

    constraint fixes

--- glpk.py
+++ glpk.py
@@ -1,5 +1,9 @@
+import math
+
 import pyomo.environ as pyo
 import numpy as np
+
+global n, K, P
 
 def read_instance(file_name):
     # Read the instance and compute the instance in dictionary
@@ -40,44 +44,47 @@ def calculate_cost(C,Y,n):
                 cost += C[i,j]*Y[i,j,k]
     return cost
 
-def Constaint9(model,i,Z):
+def Constraint_9(model,i):
     """ all nodes must belong to one and only one subset"""
-    return sum(model.Z[i,:i]) == 1
+    # return (0,sum(model.Z[i,k] for k in range(i)),1)
+    return sum(model.Z[i,k] for k in range(i)) + model.Z[0,0] == 1 + model.Z[0,0]
 
 
 def Constraint_10(model,i,j,k):
     """ Link y and z variables"""
     if(k <= min(i,j)):
         return model.Y[i,j,k] <= model.Z[i,k] 
-    return Constraint.Skip
+    return pyo.Constraint.Skip
 
 def Constraint_11(model,i,j,k):
     """ Link y and z variables"""
     if(k <= min(i,j)):
         return model.Y[i,j,k] <= model.Z[j,k]    
-    return Constraint.Skip
+    return pyo.Constraint.Skip
   
-def Constraint_12(model,k,n,P):
+def Constraint_12(model,k):
     """ Make sure a node is representative if and only if zkk is equal to one, and that each node represents at most P − 1 other nodes, hence leading to subsets withat most P nodes."""
-    return sum(model.Z[i,k] for i in range(k,n)) <= (P-1)*model.Z[k,k]
+    return sum(model.Z[i,k] for i in range(k,n)) <= (P-1) * model.Z[k,k]
     
-def constraint13(model,K,n):
+def Constraint_13(model):
     """ Each subset must be used (K in the partition)."""
     return sum(model.Z[k,k] for k in range(n)) == K
 
-def solve_lagrangian():
-    
-    instance_name = 'eil51' # 'a280.tsp'
+def solve_lagrangian(instance_name):
+    global n, K, P
+
     C = read_instance(instance_name)
+
     n = len(C)
-    K = 10
-    print(n)
+    K = 5
+    P = math.ceil(n / K)
    
     C_dict = {}
     for i in range(0,n):
         for j in range(0,n):
             C_dict[(i,j)] = C[i][j]
-    
+
+
     model = pyo.ConcreteModel()
     model.i = pyo.RangeSet(0,n-1)
     model.j = pyo.RangeSet(0,n-1)
@@ -90,14 +97,21 @@ def solve_lagrangian():
 
     model.goal = pyo.Objective(expr = calculate_cost(model.C,model.Y,n), sense = pyo.minimize)
 
-    model.Constraint_9 = pyo.Constraint(model.Z,K,n,rule=Constraint_9)
+    model.Constraint_9 = pyo.Constraint(model.i,rule=Constraint_9)
     model.Constraint_10 = pyo.Constraint(model.i,model.j,model.k,rule=Constraint_10)
     model.Constraint_11 = pyo.Constraint(model.i,model.j,model.k,rule=Constraint_11)    
-    model.Constraint_12 = pyo.Constraint(model.k,k,n,P,rule=Constraint_12)    
-    model.Constraint_13 = pyo. constraint13(model.Z,K,n) 
+    model.Constraint_12 = pyo.Constraint(model.k,rule=Constraint_12)
+    model.Constraint_13 = pyo.Constraint(rule=Constraint_13)
     opt = pyo.SolverFactory('glpk')
-    opt.solve(model) 
+    opt.solve(model)
     print(pyo.value(model.obj))
 
 
-solve_lagrangian()
+
+
+if __name__ == "__main__":
+    # matrix = read_instance("a280.tsp")
+    # matrix = read_instance("eil51.tsp")
+    # print(matrix)
+    file_name = "eil51.tsp"
+    solve_lagrangian(file_name)
