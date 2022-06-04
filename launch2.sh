3afab141fd372c1a1312f907348a2e8ff7aa37f2
antoinebertoux <antoinebertoux@gmail.com>
2022-06-04 11:38:11 +0200

    bash2

--- launch2.sh
+++ launch2.sh
@@ -4,16 +4,17 @@ FILE_LIST=("model_1.py" "model_2.py" "lagrangian_by_node.py" "lagrangian_not_nod
 INSTANCE_LIST=("a280.tsp" "eil51.tsp" "pr439.tsp" "bier127.tsp" "eil101.tsp" "eil76.tsp")
 K_LIST=("3" "4" "5")
 P_LIST=("1" "1.5" "2" "2.5")
-for FILE in ${FILE_LIST}
+for K in ${K_LIST[*]}
 	do
-	for INSTANCE in ${INSTANCE_LIST[*]}
+	for FILE in ${FILE_LIST[*]}
 	do
-		for K in ${K_LIST[*]}
+		for INSTANCE in ${INSTANCE_LIST[*]}
 		do
 			for P in ${P_LIST[*]}
 			do
-				run_job2.sh ${FILE} ${INSTANCE} ${K} ${P}
+				run_job2.sh ${FILE} ${INSTANCE} ${K} ${P} &
 			done
 		done
+		wait
 	done
 done
